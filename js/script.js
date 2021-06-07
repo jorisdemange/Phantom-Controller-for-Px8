8b2188eeca6e2405dc75c0af6eacd7d66fc01202
Joris Demange <joris.demange@gmail.com>
2021-06-07 10:45:05 +0300

    CRLF to LF

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-// SLIDE 1
-var tl = gsap.timeline({defaults:{duration: 1.2}, delay: 1.7});
+///// SLIDE 1
+var tl = gsap.timeline({ defaults: { duration: 1.2 }, delay: 1.7 });
 
-tl.from('.anim-nav1', {opacity: 0, scale: 0.9});
-tl.from('.anim-prodname', {opacity: 0, y: 30}, 0);
-tl.from('.anim-prodname', {textFillColor: '#000000', duration: 1.5, delay: 0.3}, 0)
-tl.from('.anim-nav2', {opacity: 0}, '-=.5')
+tl.from(".anim-nav1", { opacity: 0, scale: 0.9 });
+tl.from(".anim-prodname", { opacity: 0, y: 30 }, 0);
+tl.from(
+  ".anim-prodname",
+  { textFillColor: "#000000", duration: 1.5, delay: 0.3 },
+  0
+);
+tl.from(".anim-nav2", { opacity: 0 }, "-=.5");
 
-// SLIDE 2
+///// SLIDE 2
 
 /* Second video plays only when entering the viewport. 
 Conflict with jQuery, to debug
@@ -23,10 +27,9 @@ $('#video2').each(function(){
 */
 
 // Switch .active class on click
-var selector = '.slider .slide';
+var selector = ".slider .slide";
 
-$(selector).on('click', function(){
-    $(selector).removeClass('active');
-    $(this).addClass('active');
+$(selector).on("click", function () {
+  $(selector).removeClass("active");
+  $(this).addClass("active");
 });
-
